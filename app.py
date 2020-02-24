b2ddaaa4ae41891251f10a3c84779bc1ab3ddcec
Jp3Stephens <JackStephens16@yahoo.com>
2020-02-24 11:22:37 -0800

    Added a bunch of new database stuff

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from company import Company
+
+@app.route("/")
+def hello(): 
+	return "Hello World!"
+
+
--- company.py
+++ company.py
@@ -1,16 +1,30 @@
-from people import People #provide the people class
+from people import People  # provide the people class
+from app import db
 
 
-class Company:
-    def __init__(self, company_name, employees = [], location = "", company_telephone = "", social_media = {}, website = ""):
+class Company(db.Model):
+    __tablename__ = 'Company'
+    company_name = db.Column(db.String(), primary_key=True)
+    location = db.Column(db.String())
+    website = db.Column(db.String())
+
+    def __repr__(self):
+        return "<company_name {}>".format(self.company_name)
+
+    def serialize(self):
+        return {
+            "name": self.company_name,
+            "location": self.location,
+            "website": self.website
+        }
+
+    def __init__(self, company_name, employees=[], location="", company_telephone="", social_media={}, website=""):
         self.company_name = company_name
         self.employees = employees
         self.location = location
         self.company_telephone = company_telephone
         self.social_media = social_media
         self.website = website
-
-
 
     def addPerson(self, person: People):
         self.employees.append(person)
@@ -29,15 +43,12 @@ class Company:
     def addTelephone(self, company_telephone):
         self.company_telephone = company_telephone
 
-    def addSocialMedia(self, key, value):    #THIS NEEDS TO BE CHANGED. SHOULD SOCIAL MEDIA BE ITS OWN CLASS?? PROB
+    def addSocialMedia(self, key, value):  # THIS NEEDS TO BE CHANGED. SHOULD SOCIAL MEDIA BE ITS OWN CLASS?? PROB
         self.social_media[key] = value
 
-
-    def displayEmployeeNames(self): #Display people objects in company
+    def displayEmployeeNames(self):  # Display people objects in company
         for person in self.employees:
             print(person.name)
-
-
 
     def printCompanyInfo(self):
         print("Company: ", self.company_name)
@@ -55,5 +66,3 @@ class Company:
         if (self.social_media):
             for key, value in self.social_media.items():
                 print(key, ":", value)
-
-        
